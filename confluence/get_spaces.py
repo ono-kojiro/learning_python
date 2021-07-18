94f7c72ef6c110ad733a1e8fcc27f92d2d1cfab2
Kojiro ONO <ono.kojiro@gmail.com>
2021-07-18 19:28:28 +0900

    update get_spaces.py

--- confluence/get_spaces.py
+++ confluence/get_spaces.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 
 import getopt
 import json
@@ -12,83 +13,97 @@ from atlassian import Confluence
 from pprint import pprint
 
 def usage():
-    print("Usage : {0}".format(sys.argv[0]))
+	print("Usage : {0}".format(sys.argv[0]))
+
+def read_netrc() :
+	home = os.getenv('HOME')
+	filepath = '{0}/.netrc'.format(home)
+	fp = open(filepath, mode='r', encoding='utf-8')
+
+	userinfo = {}
+	record = {}
+	while 1:
+		line = fp.readline()
+		if not line :
+			break
+
+		for m in re.finditer(r'(machine|login|password)\s+([^\s]+)', line) :
+			key = m.group(1)
+			val = m.group(2)
+			record[key] = val
+
+			if 'machine' in record \
+				and 'login' in record \
+				and 'password' in record :
+
+				machine  = record['machine']
+				login    = record['login']
+				password = record['password']
+				userinfo[machine] = {
+					'login' : login,
+					'password' : password,
+				}
+				record = {}
+
+	fp.close()
+
+	return userinfo
 
 def main():
-    ret = 0
+	ret = 0
 
-    try:
-        opts, args = getopt.getopt(
-            sys.argv[1:],
-            "hvo:u:",
-            [
-                "help",
-                "version",
-                "output=",
-                "url=",
-            ]
-        )
-    except getopt.GetoptError as err:
-        print(str(err))
-        sys.exit(2)
+	try:
+		opts, args = getopt.getopt(
+			sys.argv[1:],
+			"hv",
+			[
+				"help",
+				"version",
+			]
+		)
+	except getopt.GetoptError as err:
+		print(str(err))
+		sys.exit(2)
+
+	for o, optarg in opts:
+		if o == "-v":
+			usage()
+			sys.exit(0)
+		elif o in ("-h", "--help"):
+			usage()
+			sys.exit(0)
+		else:
+			assert False, "unknown option"
 	
-    output = None
+	if ret != 0:
+		sys.exit(1)
 	
-    for o, a in opts:
-        if o == "-v":
-            usage()
-            sys.exit(0)
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit(0)
-        elif o in ("-o", "--output"):
-            output = a
-        elif o in ("-u", "--url"):
-            url = a
-        else:
-            assert False, "unknown option"
-	
-    if output == None :
-        print("no output option")
-        ret += 1
-	
-    if ret != 0:
-        sys.exit(1)
-    
+	userinfo = read_netrc()
 
-    fp = open(output, mode='w', encoding='utf-8')
+	for url in args :
+		username = ''
+		password = ''
 
-    confluence = Confluence(
-        url=url,
-        username='',
-        password=''
-    )
+		for machine in userinfo :
+			if machine in url :
+				username = userinfo[machine]['login']
+				password = userinfo[machine]['password']
 
-    spaces = confluence.get_all_spaces(start=0, limit=500, expand=None)
-    
-    #projects = jira.projects(included_archived=None)
-    #pprint(projects)
+		if username == '' or password == '' :
+			print('no username/password for {0}'.format(url))
+			sys.exit(1)
 
-    #project = jira.project('SVN')
-    #pprint(project)
+		confluence = Confluence(
+			url=url,
+			username=username,
+			password=password
+		)
 
-    #jql = 'project = SVN AND status IN ("OPEN", "REOPENED") ' + \
-    #jql = 'project = SVN AND status IN ("REOPENED") ' + \
-    #    'ORDER BY issuekey'
-    #records = jira.jql(jql)
-
-    #print(spaces);
-
-    #print(data)
-    fp.write(
-        json.dumps(
-            spaces,
-            indent=4,
-            ensure_ascii=False
-        )
-    )
-	
-    fp.close()
+		spaces = confluence.get_all_spaces(start=0, limit=500, expand=None)
+		if 'results' in spaces :
+			for result in spaces['results'] :
+				name = result['name']
+				print(name)
 	
 if __name__ == "__main__":
 	main()
