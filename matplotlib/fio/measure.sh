80f401f04b337d271e1a0885bf07f432715e3414
Kojiro ONO <ono.kojiro@gmail.com>
2021-10-01 21:50:05 +0900

    generate normal/json log

--- matplotlib/fio/measure.sh
+++ matplotlib/fio/measure.sh
@@ -1,42 +1,60 @@
 #!/bin/sh
 
-env="host"
-
-blocksizes="4K 8K 16K 32K 64K 128K 256K 512K"
+blocksizes=
+blocksizes="$blocksizes 4K 8K"
+blocksizes="$blocksizes 16K 32K"
+#blocksizes="$blocksizes 64K 128K 256K 512K"
 #blocksizes="$blocksizes 1M 2M 4M 8M 16M 32M 64M 128M 256M 512M"
-io_patterns="read write randread randwrite"
-size=64M
-ioengine=sync
+rws="read write randread randwrite"
 
-ramp_time=2s
-runtime=3s
+formats="normal json"
 
 mkdir -p out
+server=192.168.0.98
+	
+env="host"
+ioengine=sync
+size=1G
+ramp_time=2s
+runtime=5s
 
 for bs in $blocksizes; do
-  for rw in $io_patterns; do
-		title=$env-$rw-$bs
-		output=out/$title.log
-		tmppath=./fio_data-${rw}-${bs}.bin
-		echo $title
-		taskset -c 3 fio \
-			--filename=$tmppath \
-			--ioengine=$ioengine \
-			--iodepth=1 \
-			--ramp_time=$ramp_time \
-			--runtime=$runtime \
-			--direct=1 \
-			--rw=$rw \
-			--bs=$bs \
-			--size=$size \
-			--numjobs=1 \
-			--invalidate=1 \
-			--name=$title \
-			--output-format=normal \
-			--output $output 
-		echo ""
-		rm -f $tmppath
+  for rw in $rws ; do
 
-	done
+	title=$env-$rw-$bs
+	tmppath=./fio_data-${rw}-${bs}.bin
+
+	mkdir -p out
+
+	cmd="/usr/local/bin/fio"
+	cmd="$cmd --name=$title"
+	cmd="$cmd --bandwidth-log"
+	cmd="$cmd --filename=$tmppath"
+	cmd="$cmd --ioengine=$ioengine"
+	cmd="$cmd --iodepth=1"
+	cmd="$cmd --ramp_time=$ramp_time"
+	cmd="$cmd --runtime=$runtime"
+	cmd="$cmd --direct=1"
+	cmd="$cmd --rw=$rw"
+	cmd="$cmd --bs=$bs"
+	cmd="$cmd --size=$size"
+	cmd="$cmd --numjobs=1"
+	cmd="$cmd --invalidate=1"
+	cmd="$cmd --status-interval=1s"
+	#cmd="$cmd --eta=auto"
+
+	for format in $formats ; do
+		if [ "$format" = "json" ]; then
+			ext="json"
+		else
+			ext="txt"
+		fi
+
+		output="out/${title}-${format}.${ext}"
+		echo $output
+		echo $cmd --output-format=$format --output $output | \
+			ssh $server sh
+    done
+  done
 done
 
