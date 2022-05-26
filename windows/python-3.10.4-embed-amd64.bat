2ab7a647ef7b30e41e2c8cd71f58e7de964ef07e
Kojiro ONO <ono.kojiro@gmail.com>
2022-05-26 23:24:50 +0900

    add batch files

---
+++ windows/python-3.10.4-embed-amd64.bat
@@ -0,0 +1,5 @@
+PUSHD tools\python-3.10.4-embed-amd64
+
+SET PATH=%CD%;%CD%\Scripts;%PATH%
+
+POPD
---
+++ windows/strawberry-perl-5.32.1.1-64bit-portable.bat
@@ -0,0 +1,13 @@
+PUSHD tools\strawberry-perl-5.32.1.1-64bit-portable
+
+SET PATH=%CD%\perl\site\bin;%CD%\perl\bin;%CD%\c\bin;%PATH%
+
+SET TERM=
+SET PERL_JSON_BACKEND=
+SET PERL_YAML_BACKEND=
+SET PERL5LIB=
+SET PERL5OPT=
+SET PERL_MM_OPT=
+SET PERL_MB_OPT=
+
+POPD
