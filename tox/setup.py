3da90586c72592629b51955fbe493e379936b6ee
Kojiro ONO <ono.kojiro@gmail.com>
2022-11-06 17:43:51 +0900

    add tox example

---
+++ tox/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import unittest
+
+def test_suite():
+	return unittest.TestLoader().discover('tests', pattern='test_*.py')
+
+setup(
+  name='mymodule',
+  version='0.0.1',
+  py_modules=['mymodule'],
+  test_suite='setup.test_suite',
+)
+
---
+++ tox/tests/test_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import mymodule
+
+def test_simple():
+	ret = mymodule.hello()
+	assert ret == 3
+
