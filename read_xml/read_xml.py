5b5ccae94fd8790fc8d5ba3a08124ae794c33e35
Kojiro ONO <ono.kojiro@gmail.com>
2022-11-22 23:06:45 +0900

    fix bug

--- read_xml/read_xml.py
+++ read_xml/read_xml.py
@@ -1,24 +1,23 @@
-import sys
-import os
-import re
-
 import getopt
+import re
+import sys
 
 from lxml import etree
 
 def read_schema(filepath):
-    fp = open(filepath, mode='r', encoding='utf-8')
-    lines = fp.read()
-    fp.close()
+    #fp = open(filepath, mode="r", encoding="utf-8")
+    #lines = fp.read()
+    #fp.close()
 
     doc = etree.parse(filepath)
     doc.xinclude()
     schema = etree.XMLSchema(doc)
     return schema
 
+
 def print_events(parser):
     for act, elem in parser.read_events():
-        #print('{0} : {1}'.format(act, elem.tag))
+        # print('{0} : {1}'.format(act, elem.tag))
         pass
 
 def main() :
@@ -29,19 +28,19 @@ def main() :
             sys.argv[1:],
             "hvo:s:",
             [
-                "help",
-                "version",
-                "schema=",
-                "output="
+              "help",
+              "version",
+              "schema=",
+              "output="
             ]
         )
     except getopt.GetoptError as err:
         print(str(err))
         sys.exit(2)
-    
+
     output = None
     schema_xml = None
-    
+
     for option, arg in options:
         if option in ("-v", "-h", "--help"):
             usage()
@@ -52,8 +51,8 @@ def main() :
             schema_xml = arg
         else:
             assert False, "unknown option"
-    
-    if output == None :
+
+    if output == None:
         print("no output option")
         ret += 1
 
@@ -64,63 +63,53 @@ def main() :
     if ret != 0:
         sys.exit(1)
 
-    #xmlfiles = []
-    #xsdfiles = []
+    # xmlfiles = []
+    # xsdfiles = []
 
-    #for filepath in args :
+    # for filepath in args :
     #    ext = os.path.splitext(filepath)[1]
     #    if ext == '.xsd' :
     #        xsdfiles.append(filepath)
     #    elif ext == '.xml' :
     #        xmlfiles.append(filepath)
 
-    #lines = ''
-    #for filepath in xsdfiles :
+    # lines = ''
+    # for filepath in xsdfiles :
     #    print('parse {0}'.format(filepath))
     #    fp = open(filepath, mode='r', encoding='utf-8')
     #    lines += fp.read()
     #    fp.close()
 
-    #print(lines.encode('utf-8'))
-    #doc = etree.fromstring(lines)
-    #doc.xinclude()
-    #schema = etree.XMLSchema(doc)
+    # print(lines.encode('utf-8'))
+    # doc = etree.fromstring(lines)
+    # doc.xinclude()
+    # schema = etree.XMLSchema(doc)
 
     doc = etree.parse(schema_xml)
     doc.xinclude()
     schema = etree.XMLSchema(doc)
 
-    for filepath in args :
-        fp = open(filepath, mode='r', encoding='utf-8')
-        
-        parser = etree.XMLPullParser(
-            events=('start', 'end')
-        )
+    for filepath in args:
+        fp = open(filepath, encoding="utf-8")
+
+        parser = etree.XMLPullParser(events=("start", "end"))
 
         while 1:
             line = fp.readline()
-            if not line :
+            if not line:
                 break
 
-            line = re.sub(r'\r?\n?$', '', line)
-            line = re.sub(r'\t', '', line)
+            line = re.sub(r"\r?\n?$", "", line)
+            line = re.sub(r"\t", "", line)
             parser.feed(line)
             print_events(parser)
 
         fp.close()
         root = parser.close()
-        print(
-            etree.tostring(
-                root,
-                pretty_print=True,
-                encoding='unicode'
-            )
-        )
+        print(etree.tostring(root, pretty_print=True, encoding="unicode"))
         ret = schema.assertValid(root)
         print(ret)
-    
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
-
