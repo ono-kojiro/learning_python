178d6f147e2893a179083b052a97247a801ce2e9
Kojiro ONO <ono.kojiro@gmail.com>
2022-12-04 16:39:15 +0900

    separate test function of json/jsonl

--- xml2json/tests/test_junit.py
+++ xml2json/tests/test_junit.py
@@ -5,14 +5,28 @@ import pytest
 
 import subprocess
 
-script = '../_build/lib/xml2json.py'
+xml2json  = '../_build/lib/xml2json.py'
+xml2jsonl = '../_build/lib/xml2jsonl.py'
 
-def test_junit():
+def test_junit_json():
 	res = 1
 	xmlfile  = '../data/junit_report.xml'
 	jsonfile = 'junit_report.json'
 
-	res = subprocess.call(['python3', script,
+	res = subprocess.call(['python3', xml2json,
+		'-o', jsonfile,
+		xmlfile ]
+	)
+	assert res == 0
+
+def test_junit_jsonl():
+	res = 1
+	xmlfile  = '../data/junit_report.xml'
+	jsonfile = 'junit_report.jsonl'
+	index    = 'myindex'
+
+	res = subprocess.call(['python3', xml2jsonl,
+        '-i', index,
 		'-o', jsonfile,
 		xmlfile ]
 	)
