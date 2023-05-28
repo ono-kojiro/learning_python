446f829b0da166361d850b2ce0d44d8c95698a3a
Kojiro ONO <ono.kojiro@gmail.com>
2023-05-28 15:42:47 +0900

    add template of json schema

---
+++ json_schema/example01/example.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import sys
+import getopt
+
+import json
+from jsonschema import validate, ValidationError
+
+def usage():
+    print("Usage : {0} <OPTIONS> input.json".format(sys.argv[0]))
+
+def read_json(filepath):
+    fp = open(filepath, mode="r", encoding="utf-8")
+    data = json.load(fp)
+    fp.close()
+    return data
+
+def main():
+    ret = 0
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hvo:s:",
+            [
+                "help",
+                "version",
+                "output=",
+                "schema=",
+            ]
+        )
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
+
+    output = None
+    schema_json = None
+
+    for o, a in opts:
+        if o == "-v":
+            usage()
+            sys.exit(0)
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+        elif o in ("-o", "--output"):
+            output = a
+        elif o in ("-s", "--schema"):
+            schema_json = a
+        else:
+            assert False, "unknown option"
+
+    if output is not None :
+        fp = open(output, mode="w", encoding="utf-8")
+    else :
+        fp = sys.stdout
+
+    if ret != 0:
+        sys.exit(1)
+
+    if schema_json is None :
+        schema = read_json('schema.json')
+    else :
+        schema = read_json(schema_json)
+
+    for filepath in args:
+        fp.write('read {0}\n'.format(filepath))
+        data = read_json(filepath)
+        try :
+            validate(data, schema)
+        except ValidationError as e:
+            fp.write(e.message + "\n")
+
+    if output is not None :
+        fp.close()
+
+if __name__ == "__main__" :
+    main()
+
