1078f1ca9919b12bd4a8c8a72684dec26b72104b
Kojiro ONO <ono.kojiro@gmail.com>
2023-06-15 22:00:49 +0900

    add python one-liner

--- mac_address/test.sh
+++ mac_address/test.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-mac="123456778ABC"
+mac="123456789ABC"
 
 # https://perldoc.jp/docs/perl/5.36.0/perlretut.pod#Looking32ahead32and32looking32behind
 
+echo Perl
 echo $mac | perl -p -e 's/(..)(?!$)/$1:/g'
+
+echo Python
+#python3 test.py $mac
+echo 123456789ABC | python -c 'import re,sys; str=re.sub(r"(..)(?!$)","\\1:",sys.stdin.readline()); print(str)'
 
