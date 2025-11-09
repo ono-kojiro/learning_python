a42f117a01c438574c0405ed4cfc489a6333563d
Kojiro ONO <ono.kojiro@gmail.com>
2025-11-09 10:24:11 +0900

    show models

---
+++ openwebui/models/show_models.py
@@ -0,0 +1,88 @@
+#/usr/bin/env python3
+
+import sys
+import os
+
+import getopt
+
+import re
+import json
+
+import tomllib
+from OpenWebUI import Client
+
+import json
+from pprint import pprint
+
+def main():
+    ret = 0
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hvo:a:c:",
+            [
+                "help",
+                "version",
+                "output=",
+                "api-key=",
+                "config=",
+            ]
+        )
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
+
+    output = None
+
+    api_key_shrc = './api_key.shrc'
+    config_shrc  = './config.shrc'
+
+    for o, a in opts:
+        if o == "-v":
+            usage()
+            sys.exit(0)
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+        elif o in ("-o", "--output"):
+            output = a
+        elif o in ("--api-key"):
+            api_key_shrc = a
+        elif o in ("--config"):
+            config_shrc = a
+        else:
+            assert False, "unknown option"
+
+    if output is not None :
+        fp = open(output, mode='w', encoding='utf-8')
+    else :
+        fp = sys.stdout
+
+    if ret :
+        sys.exit(ret)
+
+    configs = [ api_key_shrc, config_shrc ]
+    
+    params = {}
+    for filepath in configs:
+        with open(filepath, mode='rb') as f:
+            params = params | tomllib.load(f)
+
+    base_url = params['base_url']
+    api_key  = params['api_key']
+
+    client = Client(base_url, api_key)
+
+    items = client.get_models()
+    for item in items:
+        print('{0}, {1}'.format(item['name'], item['id']))
+        pprint(item)
+
+
+    if output is not None :
+        fp.close()
+
+if __name__ == "__main__" :
+    main()
+
