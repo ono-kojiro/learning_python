d1c5b054509356ad1cacffa282b586f3f1c69e55
Kojiro ONO <ono.kojiro@gmail.com>
2025-12-31 09:57:04 +0900

    add json2dot.py

---
+++ graphviz/json2dot/db2json.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import sys
+
+import getopt
+import json
+
+import sqlite3
+
+from pprint import pprint
+
+def usage():
+    print("Usage : {0}".format(sys.argv[0]))
+
+def read_json(filepath) :
+    with open(filepath, mode='r', encoding='utf-8') as fp :
+        data = json.loads(fp.read())
+        return data
+
+def get_agents(conn) :
+    c = conn.cursor()
+    sql = 'SELECT * FROM agents_view;'
+    
+    items = []
+    rows = c.execute(sql)
+    for row in rows :
+        agent = row['agent']
+        items.append(agent)
+
+    return items
+
+def get_connections_view(conn) :
+    c = conn.cursor()
+    sql = 'SELECT * FROM connections_view;'
+    
+    items = []
+    rows = c.execute(sql)
+    for row in rows :
+        item = {
+            'agent' : row['agent'],
+            'idx'   : row['idx'],
+            'mac'   : row['mac'],
+            'ip'    : row['ip'],
+        }
+        items.append(item)
+
+    return items
+
+
+def main():
+    ret = 0
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hvo:",
+            [
+                "help",
+                "version",
+                "output="
+            ]
+        )
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
+    
+    output = None
+	
+    for o, a in opts:
+        if o == "-v":
+            usage()
+            sys.exit(0)
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+        elif o in ("-o", "--output"):
+            output = a
+        else:
+            assert False, "unknown option"
+	
+    if output is not None :
+        fp = open(output, mode='w', encoding='utf-8')
+    else :
+        fp = sys.stdout
+	
+    if ret != 0:
+        sys.exit(1)
+
+    data = {}
+
+    for database in args:
+        conn = sqlite3.connect(database)
+        conn.row_factory = sqlite3.Row
+
+        agents = get_agents(conn)
+        data['agents'] = agents
+
+        connections = get_connections_view(conn)
+        data['connections'] = connections
+
+    fp.write(
+        json.dumps(
+            data,
+            indent=4,
+            ensure_ascii=False,
+        )
+    )
+    fp.write('\n')
+
+    if output is not None :
+        fp.close()
+
+if __name__ == "__main__":
+    main()
---
+++ graphviz/json2dot/json2dot.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+
+import sys
+
+import getopt
+import json
+
+import re
+import yaml
+
+import copy
+
+from pprint import pprint
+
+#from Agent import Agent
+
+def usage():
+    print("Usage : {0}".format(sys.argv[0]))
+
+def read_json(filepath) :
+    with open(filepath, mode='r', encoding='utf-8') as fp :
+        data = json.loads(fp.read())
+        return data
+
+def read_yaml(filepath):
+    fp = open(filepath, mode="r", encoding="utf-8")
+    tmp = yaml.load(fp, Loader=yaml.loader.SafeLoader)
+    data = copy.deepcopy(tmp)
+    fp.close()
+
+    return data
+
+def main():
+    ret = 0
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hvo:",
+            [
+                "help",
+                "version",
+                "output="
+            ]
+        )
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
+    
+    output = None
+	
+    for o, a in opts:
+        if o == "-v":
+            usage()
+            sys.exit(0)
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+        elif o in ("-o", "--output"):
+            output = a
+        else:
+            assert False, "unknown option"
+	
+    if output is not None :
+        fp = open(output, mode='w', encoding='utf-8')
+    else :
+        fp = sys.stdout
+	
+    if ret != 0:
+        sys.exit(1)
+
+    configs = read_yaml('./config.yml')
+
+    header = '''
+digraph mygraph {
+    rankdir = "LR";
+    ordering = out;
+
+    nodesep = "0.1";
+    ranksep = "0.1";
+
+    splines = true;
+    overlap = false;
+    '''
+
+    footer = '''
+}
+    '''
+
+    fp.write(header)
+    fp.write('\n')
+
+    data = {}
+    for jsonfile in args:
+        data = read_json(jsonfile)
+        agent_ips = data['agents']
+
+        conns = data['connections']
+
+        # draw agents
+        for agent_ip in agent_ips :
+            #agent = Agent(agent_ip)
+
+            lines = []
+            cluster = re.sub(r'\.', '_', agent_ip)
+            lines.append('    subgraph cluster_{0} {{'.format(cluster))
+            lines.append('        label = "{0}";'.format(agent_ip))
+
+            lines.append('        node_{0}_image ['.format(cluster))
+            lines.append('            shape=none')
+            lines.append('            image="icons/doc_jpg/small_hub.png"')
+            lines.append('            label=none')
+            lines.append('        ];'.format(cluster))
+            
+            idxs = {}
+            for conn in conns :
+                if conn['agent'] == agent_ip :
+                    idx = conn['idx']
+                    idxs[idx] = 1
+       
+            for idx in idxs:
+                line = '        '
+                line += 'node_{0}_port{1} ['.format(cluster, idx)
+                line += ' shape=rectangle label="{0}"'.format(idx)
+                lines.append(line)
+                    
+                line = '            '
+                line += 'fixedsize=true'
+                line += ' width=0.3 height=0.3 ];'
+                lines.append(line)
+
+            lines.append('        {')
+            lines.append('            rank = same;')
+
+            default_idx = configs['default_idx'][agent_ip]
+
+            for idx in idxs:
+                if default_idx != idx :
+                    line = '            '
+                    line += 'node_{0}_port{1};'.format(cluster, idx)
+                    lines.append(line)
+            lines.append('        }')
+            lines.append('')
+
+            line =  '        '
+            line += 'node_{0}_port{1} -> node_{0}_image;'.format(cluster, default_idx)
+            lines.append(line)
+
+            for idx in idxs:
+                if default_idx == idx :
+                    continue
+
+                line  = '        '
+                line += 'node_{0}_image -> node_{0}_port{1};'.format(cluster, idx)
+                lines.append(line)
+            lines.append('')
+
+            #for item in items :
+            #    if item['agent'] == agent :
+            #        idx = item['idx']
+            #        mac = item['mac']
+            #        ip  = item['ip']
+
+            # end of subgraph
+            lines.append('    }')
+
+            for line in lines :
+                fp.write(line)
+                fp.write('\n')
+            fp.write('\n')
+
+
+        agent_list = {}
+        for agent_ip in agent_ips :
+            agent_list[agent_ip] = 1
+      
+        pprint(agent_list)
+
+        fp.write('   // plot other node\n')
+        
+        mac_list = {}
+        for conn in conns :
+            mac   = conn['mac']
+            ip    = conn['ip']
+            if ip in agent_list :
+                continue
+            mac_list[mac] = ip
+
+        # plot other node
+        #for conn in conns :
+        for mac in mac_list :
+            #agent = conn['agent']
+            #idx   = conn['idx']
+            #mac   = conn['mac']
+            #ip    = conn['ip']
+
+            #if ip in agent_list :
+            #    continue
+           
+            ip = mac_list[mac]
+            lines = []
+            if ip :
+                label = ip
+                cluster = re.sub(r'\.', '_', ip)
+            else :
+                label = mac
+                cluster = re.sub(r'\:', '_', mac)
+
+            lines.append('    subgraph cluster_{0} {{'.format(cluster))
+            lines.append('        label = "{0}";'.format(label))
+            lines.append('        node_{0}_image ['.format(cluster))
+            lines.append('            shape=none')
+            lines.append('            image="icons/doc_jpg/pc.png"')
+            lines.append('            label=""')
+            lines.append('            fixedsize=true')
+            lines.append('            imagescale=height')
+            lines.append('        ];'.format(cluster))
+            lines.append('        node_{0}_port1 ['.format(cluster))
+            lines.append('            shape=box')
+            lines.append('            label="1"')
+            lines.append('            fixedsize=true')
+            lines.append('            width=0.3')
+            lines.append('            height=0.3')
+            lines.append('        ];')
+            lines.append('        node_{0}_dummy ['.format(cluster))
+            lines.append('            style=invisible')
+            lines.append('            shape=box')
+            lines.append('            label=""')
+            lines.append('            fixedsize=true')
+            lines.append('            width=0.3')
+            lines.append('            height=0.3')
+            lines.append('        ];')
+            lines.append('        node_{0}_port1 -> node_{0}_image [color=none, weight=10];'.format(cluster))
+            lines.append('        node_{0}_image -> node_{0}_dummy [color=none ];'.format(cluster))
+            lines.append('     };')
+
+            for line in lines :
+                fp.write(line)
+                fp.write('\n')
+            fp.write('\n')
+
+
+        # agents
+        # draw edge
+        fp.write('   // draw edge\n')
+        for conn in conns :
+            agent = conn['agent']
+            idx   = conn['idx']
+            mac   = conn['mac']
+            ip    = conn['ip']
+            
+            default_idx = configs['default_idx'][agent]
+            if idx == default_idx :
+                continue
+            
+            if ip and ip in agent_list :
+                print('DEBUG: ip {0} is agent, continue'.format(ip))
+                continue
+            else :
+                #print('DEBUG: ip {0} is not agent'.format(ip))
+                pass
+
+
+            src_cluster = re.sub(r'\.', '_', agent)
+            src = "node_{0}_port{1}".format(src_cluster, idx)
+
+            if ip :
+                dst_cluster = re.sub(r'\.', '_', ip)
+            else :
+                dst_cluster = re.sub(r'\:', '_', mac)
+
+            dst = "node_{0}_port1".format(dst_cluster)
+            line = "    {0} -> {1};".format(src, dst)
+            fp.write(line + '\n')
+
+    fp.write(footer)
+    fp.write('\n')
+
+    if output is not None :
+        fp.close()
+
+if __name__ == "__main__":
+    main()
