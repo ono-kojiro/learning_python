ce827ccbb703b328e98ca08535822ca767ae541a
Kojiro ONO <ono.kojiro@gmail.com>
2026-01-01 10:04:53 +0900

    add Terminal class

--- graphviz/json2dot/json2dot.py
+++ graphviz/json2dot/json2dot.py
@@ -14,6 +14,7 @@ from pprint import pprint
 
 #from Agent import Agent
 from Edge import Edge
+from Terminal import Terminal
 
 def usage():
     print("Usage : {0}".format(sys.argv[0]))
@@ -214,39 +215,8 @@ digraph mygraph {
             else :
                 dst_port = "1"
 
-            lines.append('    subgraph cluster_{0} {{'.format(cluster))
-            lines.append('        label = "{0}";'.format(label))
-            lines.append('        node_{0}_image ['.format(cluster))
-            lines.append('            shape=none')
-            lines.append('            image="icons/doc_jpg/pc.png"')
-            lines.append('            label=""')
-            lines.append('            fixedsize=true')
-            lines.append('            imagescale=height')
-            lines.append('        ];'.format(cluster))
-            lines.append('        node_{0}_port{1} ['.format(cluster, dst_port))
-            lines.append('            shape=box')
-            lines.append('            label="1"')
-            lines.append('            fixedsize=true')
-            lines.append('            width=0.3')
-            lines.append('            height=0.3')
-            lines.append('        ];')
-            lines.append('        node_{0}_dummy ['.format(cluster))
-            lines.append('            style=invisible')
-            lines.append('            shape=box')
-            lines.append('            label=""')
-            lines.append('            fixedsize=true')
-            lines.append('            width=0.3')
-            lines.append('            height=0.3')
-            lines.append('        ];')
-            lines.append('        node_{0}_port1 -> node_{0}_image [color=none, weight=100, len=0.3];'.format(cluster))
-            lines.append('        node_{0}_image -> node_{0}_dummy [color=none ];'.format(cluster))
-            lines.append('     };')
-
-            for line in lines :
-                fp.write(line)
-                fp.write('\n')
-            fp.write('\n')
-
+            terminal = Terminal(ip, mac, dst_port)
+            terminal.print(fp)
 
         # agents
         # draw edge
