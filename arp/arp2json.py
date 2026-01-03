68ae4ccf61a5c7e209ae9d61b65e2c4f27bf115b
Kojiro ONO <ono.kojiro@gmail.com>
2026-01-03 16:24:49 +0900

    simplify arp2json.py

--- arp/arp2json.py
+++ arp/arp2json.py
@@ -4,132 +4,110 @@ import sys
 import re
 
 import getopt
+import yaml
 
+from pprint import pprint
 import json
 
-STATE_INIT   = 0
-STATE_HEADER = 1
-STATE_RECORD = 2
+import ipaddress
 
 def usage():
-    print(f"Usage : {sys.argv[0]} -o <output> <input>...")
+    print("Usage : {0}".format(sys.argv[0]))
+
+def read_json(jsonfile) :
+    fp = open(jsonfile, mode='r', encoding='utf-8')
+    data = json.load(fp)
+    fp.close()
+    return data
+
+def is_valid_ipv4(addr) :
+    try :
+        ipaddress.IPv4Address(addr)
+        return True
+    except :
+        return False
 
 def main():
     ret = 0
 
     try:
-        options, args = getopt.getopt(
+        opts, args = getopt.getopt(
             sys.argv[1:],
             "hvo:",
             [
                 "help",
                 "version",
-                "output=",
-            ],
+                "output="
+            ]
         )
     except getopt.GetoptError as err:
         print(str(err))
-        sys.exit(1)
-
+        sys.exit(2)
+    
     output = None
-
-    for option, optarg in options:
-        if option == "-v":
+    
+    for o, a in opts:
+        if o == "-v":
             usage()
-            sys.exit(1)
-        elif option in ("-h", "--help"):
+            sys.exit(0)
+        elif o in ("-h", "--help"):
             usage()
-            sys.exit(1)
-        elif option in ("-o", "--output"):
-            output = optarg
+            sys.exit(0)
+        elif o in ("-o", "--output"):
+            output = a
         else:
             assert False, "unknown option"
-
-    if output is not None :
+    
+    if output is not None:
         fp = open(output, mode="w", encoding="utf-8")
     else :
         fp = sys.stdout
 
     if ret != 0:
-        sys.exit(ret)
-
-    config  = {}
-    records = {}
+        sys.exit(1)
+   
+    mac2ip = {}
 
     for filepath in args:
-        fp_in = open(filepath, mode="r", encoding="utf-8")
-
-        state = STATE_INIT
-
-        while 1 :
+        fp_in = open(filepath, mode='r', encoding='utf-8')
+        while True:
             line = fp_in.readline()
             if not line :
                 break
 
             line = re.sub(r'\r?\n?$', '', line)
-
-            # change state
-            if state == STATE_INIT :
-                if line == 'ARP_SCAN_BEGIN':
-                    state = STATE_HEADER
-                pass
-            elif state == STATE_HEADER :
-                if re.search(r'^Starting arp-scan', line):
-                    state = STATE_RECORD
-                pass
-            elif state == STATE_RECORD :
-                if re.search(r'^ARP_SCAN_END', line):
-                    state = STATE_RECORD
-                pass
-            
-            # read value
-            if state == STATE_INIT :
-                pass
-            elif state == STATE_HEADER :
-                tokens = re.split(r', ', line)
-                for token in tokens:
-                    m = re.search(r'(.+):\s+(.+)', token)
-                    if m :
-                        key = m.group(1)
-                        val = m.group(2)
-                        config[key] = val
-            elif state == STATE_RECORD :
-                m = re.search(r'([\d\.]+)\s+([0-9A-Fa-f:]{17})\s+(.+?)', line)
-                if m :
-                    ip_addr = m.group(1)
-                    mac_addr = m.group(2)
-                    vendor   = m.group(3)
-                    records[ip_addr] = mac_addr
-                pass
-            
-            # post proc
-            if state == STATE_INIT :
-                pass
-            elif state == STATE_HEADER :
-                pass
-            elif state == STATE_RECORD :
-                pass
+            m = re.search(r'^(\S+)\s+(\S+)\s+(.+)', line)
+            if m :
+                ip = m.group(1)
+                mac = m.group(2)
+                vendor = m.group(3)
+                if is_valid_ipv4(ip) :
+                    mac2ip[mac] = {
+                        'ip' : ip,
+                        'vendor' : vendor,
+                    }
 
         fp_in.close()
 
-    #print(config)
-    #print(records)
-
-    hostname = config['HOSTNAME']
+    #yaml.dump(data,
+    #    fp,
+    #    allow_unicode=True,
+    #    default_flow_style=False,
+    #    sort_keys=True,
+    #)
 
     fp.write(
         json.dumps(
             {
-                hostname : {
-                    'config' : config,
-                    'records': records,
-                }
+                "arp-scan" : mac2ip,
             },
             indent=4,
             ensure_ascii=False,
             sort_keys=True,
         )
     )
+    
+    fp.write('\n')
 
     if output is not None:
         fp.close()
