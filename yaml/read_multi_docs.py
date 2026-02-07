f4867277d33fff77a3d94d9946990b8fa3efa781
Kojiro ONO <ono.kojiro@gmail.com>
2026-02-07 15:10:28 +0900

    add read_multi_docs.py

---
+++ yaml/read_multi_docs.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import sys
+
+import getopt
+import yaml
+
+from pprint import pprint
+
+def read_yaml(filepath):
+    fp = open(filepath, mode="r", encoding="utf-8")
+    docs = yaml.load_all(fp, Loader=yaml.loader.SafeLoader)
+
+    #pprint(docs)
+
+    data = []
+    for items in docs:
+        for item in items:
+            #pprint(item)
+            data.append(item)
+
+    fp.close()
+    return data
+
+def main() :
+    ret = 0
+
+    try:
+        options, args = getopt.getopt(
+            sys.argv[1:],
+            "hvo:",
+            [
+              "help",
+              "version",
+              "output="
+            ]
+        )
+    except getopt.GetoptError as err:
+        print(str(err))
+        sys.exit(2)
+
+    output = None
+
+    for option, arg in options:
+        if option in ("-v", "-h", "--help"):
+            usage()
+            sys.exit(0)
+        elif option in ("-o", "--output"):
+            output = arg
+        else:
+            assert False, "unknown option"
+
+    if output is not None:
+        fp = open(output, mode="w", encoding="utf-8")
+    else :
+        fp = sys.stdout
+
+    if ret != 0:
+        sys.exit(1)
+
+    for filepath in args :
+      data = read_yaml(filepath)
+      print(data)
+
+    if output is not None:
+        fp.close()
+
+if __name__ == "__main__":
+    main()
