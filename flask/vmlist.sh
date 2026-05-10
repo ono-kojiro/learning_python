3e00b4e1d7a5d20f68a617a62e4df69ef054a9df
Kojiro ONO <ono.kojiro@gmail.com>
2026-05-10 19:19:43 +0900

    update requirements.txt

--- flask/vmlist.sh
+++ flask/vmlist.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
-sudo vm list | python3 vmlist.py - > vmlist.json
+vms=`virsh list --all --name`
+for vm in $vms; do
+  virsh dumpxml $vm
+done
+#sudo vm list | python3 vmlist.py - > vmlist.json
 
