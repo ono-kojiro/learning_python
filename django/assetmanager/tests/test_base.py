567a9a19b60e70b75e13496b37d65e68783a7525
Kojiro ONO <ono.kojiro@gmail.com>
2026-05-27 23:19:46 +0900

    update test

--- django/assetmanager/tests/test_base.py
+++ django/assetmanager/tests/test_base.py
@@ -15,7 +15,8 @@ def test_add_devices(configs):
         print(url, file=sys.stderr)
         print(item, file=sys.stderr)
 
-        res = requests.get("{0}?device_id={1}".format(url, item['device_id']))
+        params = { "serial_number": item["serial_number"] }
+        res = requests.get("{0}".format(url), params=params)
         if res.status_code == 200 and len(res.json()) > 0:
             continue
 
@@ -46,7 +47,12 @@ def test_add_netifs(configs):
         json = item
         print(url, file=sys.stderr)
         print(item, file=sys.stderr)
-        res = requests.get("{0}?netif_id={1}".format(url, item['netif_id']))
+
+        params = {
+            "devicd": item["device"],
+            "name":   item["name"],
+        }
+        res = requests.get("{0}".format(url), params=params)
         if res.status_code == 200 and len(res.json()) > 0:
             continue
         
