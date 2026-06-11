f7b92c56ac3b4fce79c6bb3e55e163538d9a3657
Kojiro ONO <ono.kojiro@gmail.com>
2026-06-11 21:48:41 +0900

    add VirtualIP module

--- opnsense/src/opnsense_python/__init__.py
+++ opnsense/src/opnsense_python/__init__.py
@@ -1,8 +1,10 @@
 from .client import OPNsenseClient
 from .firewall import FirewallAPI
+from .virtual_ip import VirtualIPAPI
 
 __all__ = [
     "OPNsenseClient",
     "FirewallAPI",
+    "VirtualIPAPI",
 ]
 
